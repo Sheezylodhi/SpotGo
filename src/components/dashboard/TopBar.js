e5b9c41ac0d9956d354a2ff065ad55716221f92f
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2026-09-03 23:55:43 +0500

    Fixed

--- src/components/dashboard/TopBar.js
+++ src/components/dashboard/TopBar.js
@@ -147,7 +147,7 @@ export default function TopBar({ onMenuClick }) {
                 text-white
               "
             >
-              MS
+              MA
             </div>
 
             {/* User Info */}
